5f788d0a6c8b5ead92da0042508ddd2f1ec9901f
meetasati07 <mmasati1976@gmail.com>
2025-08-10 19:32:05 +0530

    Backend Production

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
